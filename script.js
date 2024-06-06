72c686147bc8631c18e8d8bca059d581f67a194f
stefania m <shin3qi@gmail.com>
2024-06-06 22:04:56 +0300

    fixed

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const input = document.getElementById('input');
+const qoutes =[
+  "If society was so sane, no one would have been insane." ,
+  "All is Mind. The Universe is Mental." ,
+  "I was ashamed of myself when I realized life was a costume party and I attended with my real face.",
+  "Pain and suffering are always inevitable for a large intelligence and a deep heart.",
+  "To go wrong in one's own way is better than to go right in someone else's.",
+  "What is hell? I maintain that it is the suffering of being unable to love.",
+  "But how could you live and have no story to tell?",
+  "Man is sometimes extraordinarily, passionately, in love with suffering...",
+  "I think the devil doesn't exist, but man has created him, he has created him in his own image and likeness.",
+  "It is better to be unhappy and know the worst, than to be happy in a fool's paradise.",
+  "Your worst sin is that you have destroyed and betrayed yourself for nothing."
 
-function reverseString(str) {
- return str.split("").reverse().join("")
-}
+]
 
+const usedIndexes = new Set()
+const quoteTag = document.getElementById('quote')
 
-function check() {
-  const value = input.value;
-  const reverse = reverseString(value)
+  function generateQuote () {
+    if (usedIndexes.size >= qoutes.length) {
+      usedIndexes.clear()
+    }
 
+    while (true) {
+   const randomIdx = Math.floor(Math.random() * qoutes.length);
 
-if (value === reverse){
-  alert("P A L I N D R O M E")
-} else {
-  alert("Not today!")
-}
+if (usedIndexes.has(randomIdx)) continue
 
-input.value = ""
-}
+   const quote = qoutes[randomIdx]
+   quoteTag.innerHTML = quote;
+   usedIndexes.add(randomIdx)
+   break
+   }
+  }
